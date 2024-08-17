a94650f12cf8aa5cb41a492da75b6ab60484008f
troyyxk <xingkunyin@hotmail.com>
2024-08-17 23:50:38 +0800

    Revert "finish walkthrough, stuck at where is delta tensor at LMCacheEngine"
    This reverts commit dc674cd43e0a911b6cc68abbb9f7defb8ffe8da4.

--- experiment_log/everytime_setup.sh
+++
@@ -1,6 +0,0 @@
-source /etc/network_turbo
-cd /root/autodl-tmp/CacheGen
-export HF_ENDPOINT=https://hf-mirror.com
-export HF_HOME=~/autodl-tmp/huggingface
-export SAVE_DIR=./tmp/
-conda activate cachegen
